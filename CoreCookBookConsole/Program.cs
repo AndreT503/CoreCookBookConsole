4d26e9d15e58f87a15eee0317edc8eea0185ae5b
AndreT503 <atournier3@msn.com>
2020-01-24 13:31:07 -0800

    Add project files.

---
+++ CoreCookBookConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreCookBookConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
